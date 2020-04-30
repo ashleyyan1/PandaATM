d9a601345c5ff751040b39a84527da1cc92f2e8f
jeff Liem <liemjo24@hotmail.com>
2020-04-30 00:46:47 -0700

    add counters for transaction screens

--- PandaATMGUI/app/src/main/java/com/group7/pandaatm/cashDepositScreen.java
+++ PandaATMGUI/app/src/main/java/com/group7/pandaatm/cashDepositScreen.java
@@ -3,37 +3,127 @@ package com.group7.pandaatm;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class cashDepositScreen extends AppCompatActivity {
+
+    TextView oneValue;
+    TextView fiveValue;
+    TextView tenValue;
+    TextView twentyValue;
+    TextView fiftyValue;
+    TextView hundredValue;
+
+    //initialize counters to zero
+    int oneCounter = 0;
+    int fiveCounter = 0;
+    int tenCounter = 0;
+    int twentyCounter = 0;
+    int fiftyCounter = 0;
+    int hundredCounter = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cash_deposit_screen);
 
+        oneValue = findViewById(R.id.oneCounter);
+        fiveValue = findViewById(R.id.fiveCounter);
+        tenValue = findViewById(R.id.tenCounter);
+        twentyValue = findViewById(R.id.twentyCounter);
+        fiftyValue = findViewById(R.id.fiftyCounter);
+        hundredValue = findViewById(R.id.hundredCounter);
+
+
         //initialize click listeners
-        findViewById(R.id.clearButton).setOnClickListener(buttonClickListener);
         findViewById(R.id.enterButton).setOnClickListener(buttonClickListener);
         findViewById(R.id.cancelButton).setOnClickListener(buttonClickListener);
 
+        findViewById(R.id.addOne).setOnClickListener(buttonClickListener);
+        findViewById(R.id.subOne).setOnClickListener(buttonClickListener);
+        findViewById(R.id.addFive).setOnClickListener(buttonClickListener);
+        findViewById(R.id.subFive).setOnClickListener(buttonClickListener);
+        findViewById(R.id.addTen).setOnClickListener(buttonClickListener);
+        findViewById(R.id.subTen).setOnClickListener(buttonClickListener);
+        findViewById(R.id.addTen).setOnClickListener(buttonClickListener);
+        findViewById(R.id.addTwenty).setOnClickListener(buttonClickListener);
+        findViewById(R.id.subTwenty).setOnClickListener(buttonClickListener);
+        findViewById(R.id.addFifty).setOnClickListener(buttonClickListener);
+        findViewById(R.id.subFifty).setOnClickListener(buttonClickListener);
+        findViewById(R.id.addHundred).setOnClickListener(buttonClickListener);
+        findViewById(R.id.subHundred).setOnClickListener(buttonClickListener);
+
+
     }
+
 
     private View.OnClickListener buttonClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
             switch (v.getId()) {
-                case R.id.clearButton:     //if clear button is clicked, clear text fields
-                    EditText cashAmt = findViewById(R.id.cashamt);
-                    cashAmt.getText().clear();
+                case R.id.addOne:                   //maybe needs a limit for adding
+                    oneCounter++;
+                    oneValue.setText("("+ oneCounter +")");
                     break;
-                case R.id.enterButton:
-//                case R.id.enterButton:
-//                    Intent enter = new Intent(cashDepositScreen.this, checkDepositScreen.class);
-//                    startActivity(deposit);
-//                    break;
+                case R.id.subOne:
+                    if (oneCounter > 0) {
+                        oneCounter--;
+                        oneValue.setText("("+ oneCounter +")");
+                    }
+                    break;
+                case R.id.addFive:
+                    fiveCounter++;
+                    fiveValue.setText("("+ fiveCounter +")");
+                    break;
+                case R.id.subFive:
+                    if (fiveCounter > 0) {
+                        fiveCounter--;
+                        fiveValue.setText("("+ fiveCounter +")");
+                    }
+                    break;
+                case R.id.addTen:
+                    tenCounter++;
+                    tenValue.setText("("+ tenCounter +")");
+                    break;
+                case R.id.subTen:
+                    if (tenCounter > 0) {
+                        tenCounter--;
+                        tenValue.setText("("+ tenCounter +")");
+                    }
+                    break;
+                case R.id.addTwenty:
+                    twentyCounter++;
+                    twentyValue.setText("("+ twentyCounter +")");
+                    break;
+                case R.id.subTwenty:
+                    if (twentyCounter> 0) {
+                        twentyCounter--;
+                        twentyValue.setText("("+ twentyCounter +")");
+                        break;
+                    }
+                case R.id.addFifty:
+                    fiftyCounter++;
+                    fiftyValue.setText("("+ fiftyCounter +")");
+                    break;
+                case R.id.subFifty:
+                    if (fiftyCounter > 0) {
+                        fiftyCounter--;
+                        fiftyValue.setText("("+ fiftyCounter +")");
+                    }
+                    break;
+                case R.id.addHundred:
+                    hundredCounter++;
+                    hundredValue.setText("("+ hundredCounter +")");
+                    break;
+                case R.id.subHundred:
+                    if (hundredCounter > 0) {
+                        hundredCounter--;
+                        hundredValue.setText("("+ hundredCounter +")");
+                    }
+                    break;
                 case R.id.cancelButton:         //if previous button is clicked, go to deposit choice screen
                     Intent cancel = new Intent(cashDepositScreen.this, transactionScreen.class);
                     startActivity(cancel);
--- PandaATMGUI/app/src/main/java/com/group7/pandaatm/loginScreen.java
+++ PandaATMGUI/app/src/main/java/com/group7/pandaatm/loginScreen.java
@@ -2,12 +2,17 @@ package com.group7.pandaatm;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
-
+import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 public class loginScreen extends AppCompatActivity {
+    private EditText cardNumber;
+    private EditText pinNumber;
+    private Button buttonConfirm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,26 +23,71 @@ public class loginScreen extends AppCompatActivity {
         findViewById(R.id.loginButton).setOnClickListener(buttonClickListener);
         findViewById(R.id.clearButton).setOnClickListener(buttonClickListener);
 
+        cardNumber = findViewById(R.id.cardNumberText);
+        pinNumber = findViewById(R.id.pinText);
+        buttonConfirm = findViewById(R.id.loginButton);
+
+        cardNumber.addTextChangedListener(loginWatcher);
+        pinNumber.addTextChangedListener(loginWatcher);
+
     }
+    private TextWatcher loginWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            String cardNumberr = cardNumber.getText().toString().trim();
+            String pinNumberr = pinNumber.getText().toString().trim();
+
+            buttonConfirm.setEnabled(!cardNumberr.isEmpty() && !pinNumberr.isEmpty());
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+
+        }
+    };
+
 
     private View.OnClickListener buttonClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
+            int number = 12345;
+            int pin = 1234;
+            EditText cardNum = findViewById(R.id.cardNumberText);
+            EditText pinNum = findViewById(R.id.pinText);
+
             switch (v.getId()) {
-                case R.id.loginButton:      //if login button is clicked, go to transaction screen
+                case R.id.loginButton:
                     Intent login = new Intent(loginScreen.this, transactionScreen.class);
                     startActivity(login);
                     break;
+                    // if login button is clicked, go to transaction screen
+                    //check if fields are null
+//                    if (cardNum.getText().toString().equals(number) && pinNum.getText().toString().equals(pin)) {
+//                        Intent login = new Intent(loginScreen.this, transactionScreen.class);
+//                        startActivity(login);
+//                    } else {
+//                        AlertDialog.Builder errorAlert = new AlertDialog.Builder(loginScreen.this);
+//                        errorAlert.setMessage("Wrong card number or PIN.");
+//                        errorAlert.setTitle("Error Message...");
+//                        errorAlert.setPositiveButton("OK", null);
+//                        errorAlert.setCancelable(true);
+//                        errorAlert.create().show();
+//                    }
+//                    break;
                 case R.id.clearButton:      //if clean button is clicked, clear text fields
-                    EditText cardNum = findViewById(R.id.cardNumberText);
-                    EditText pinNum = findViewById(R.id.pinText);
                     cardNum.getText().clear();
                     pinNum.getText().clear();
                     break;
                 default:
                     break;
-
             }
+
         }
+
     };
 }
